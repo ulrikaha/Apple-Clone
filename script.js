8cd395ec4c11cc9a84da91cc5b35d675ca158cb9
Elin <es.fredriksson@gmail.com>
2022-12-19 15:02:27 +0100

    bugg fix

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+//  Responsive CSS slider 
+let sliderUL = document.querySelector('.s-type-3 > .s-content');
+let sliderLength = sliderUL.childElementCount;
+let leftControl = document.querySelector('.s-type-3 .left-control');
+let rightControl = document.querySelector('.s-type-3 .right-control');
+let getCurrentIndex = () => Number(sliderUL.getAttribute('data-index'));
+let setCurrentIndex = index => {
+  console.log(index);
+  sliderUL.setAttribute('data-index', index);
+  sliderUL.style = `transform: translateX(-${(index - 1) (100 / sliderLength)}%)`
+}
+let onControlClick = (direction) => {
+  let index = getCurrentIndex();
+  index = direction === 'left'
+    ? (index === 1 ? sliderLength : index - 1)
+    : (index === sliderLength ? 1 : index + 1)
+  setCurrentIndex(index);
+}
+
+leftControl.addEventListener('click', () => onControlClick('left'));
+
+rightControl.addEventListener('click', () => onControlClick('right'));
+
 const hamburger = document.querySelector(".hamburger");
 const navBarLinks = document.querySelector(".navbar-links-header");
 const shopMenu = document.querySelector(".menulinks-header");
@@ -16,3 +39,4 @@ shopIcon.addEventListener("click", () => {
 shopIconDesktop.addEventListener("click", () => {
     shopMenu.classList.toggle("active");
 })
+
