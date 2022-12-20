efa67050e7c08d06a2075baf37ccbdcccdad58c7
Elin <es.fredriksson@gmail.com>
2022-12-20 10:34:35 +0100

    JS fixed

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//  Responsive CSS slider 
+/* Responsive CSS slider */
 let sliderUL = document.querySelector('.s-type-3 > .s-content');
 let sliderLength = sliderUL.childElementCount;
 let leftControl = document.querySelector('.s-type-3 .left-control');
@@ -7,7 +7,7 @@ let getCurrentIndex = () => Number(sliderUL.getAttribute('data-index'));
 let setCurrentIndex = index => {
   console.log(index);
   sliderUL.setAttribute('data-index', index);
-  sliderUL.style = `transform: translateX(-${(index - 1) (100 / sliderLength)}%)`
+  sliderUL.style = `transform: translateX(-${(index - 1) * (100 / sliderLength)}%)`
 }
 let onControlClick = (direction) => {
   let index = getCurrentIndex();
@@ -20,23 +20,21 @@ let onControlClick = (direction) => {
 leftControl.addEventListener('click', () => onControlClick('left'));
 
 rightControl.addEventListener('click', () => onControlClick('right'));
-
 const hamburger = document.querySelector(".hamburger");
 const navBarLinks = document.querySelector(".navbar-links-header");
 const shopMenu = document.querySelector(".menulinks-header");
 const shopIcon = document.querySelector(".shop-logo-header");
 const shopIconDesktop = document.querySelector(".shop-logo-header-desktop");
-
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     navBarLinks.classList.toggle("active");
 })
-
 shopIcon.addEventListener("click", () => {
     shopMenu.classList.toggle("active");
 })
 
+
+
 shopIconDesktop.addEventListener("click", () => {
     shopMenu.classList.toggle("active");
 })
-
